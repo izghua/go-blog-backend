1d9fe45ee4b6e319095e32301550ceecd4efe956
叶落山城 <2067930913@qq.com>
2019-01-07 23:15:19 +0800

    :bug: deal with eslint

--- src/components/post/expand-post.vue
+++ src/components/post/expand-post.vue
@@ -5,35 +5,35 @@
 </style>
 <template>
     <div>
-        <i-row class="expand-row">
-            <i-col span="8">
+        <Row class="expand-row">
+            <Col span="8">
                 <span class="expand-key">Job: </span>
                 <span class="expand-value">{{ row.job }}</span>
-            </i-col>
+            </Col>
 
-            <i-col span="8">
+            <Col span="8">
                 <span class="expand-key">Interest: </span>
                 <span class="expand-value">{{ row.interest }}</span>
-            </i-col>
-            <i-col span="8">
+            </Col>
+            <Col span="8">
                 <span class="expand-key">Birthday: </span>
                 <span class="expand-value">{{ row.birthday }}</span>
-            </i-col>
-        </i-row>
-        <i-row>
-            <i-col span="8">
+            </Col>
+        </Row>
+        <Row>
+            <Col span="8">
                 <span class="expand-key">Favorite book: </span>
                 <span class="expand-value">《{{ row.book }}》</span>
-            </i-col>
-            <i-col span="8">
+            </Col>
+            <Col span="8">
                 <span class="expand-key">Favorite movie: </span>
                 <span class="expand-value">{{ row.movie }}</span>
-            </i-col>
-            <i-col span="8">
+            </Col>
+            <Col span="8">
                 <span class="expand-key">Favorite music: </span>
                 <span class="expand-value">{{ row.music }}</span>
-            </i-col>
-        </i-row>
+            </Col>
+        </Row>
 
     </div>
 </template>
